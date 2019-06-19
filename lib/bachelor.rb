b81966fd15feeee3523fa7bd5151092026fc0b4f
Samantha Kane <s.kane133@gmail.com>
2019-06-19 20:42:38 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -13,6 +13,8 @@ end
 
 def get_contestant_name(data, occupation)
  data.each do |names|
+   names.each do |key, value|
+     if value == "occupation"
 end
 
 def count_contestants_by_hometown(data, hometown)
