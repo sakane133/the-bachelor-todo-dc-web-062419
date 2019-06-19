1941f0d73a21593945a7f1a629e3bcec945c7b03
Samantha Kane <s.kane133@gmail.com>
2019-06-19 20:55:10 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -38,7 +38,14 @@ def count_contestants_by_hometown(data, hometown)
 end
 
 def get_occupation(data, hometown)
-  # code here
+   data.each do |season, value|
+   value.each do |person|
+     person.each do |key, v|
+     if v == hometown
+   end
+ end
+ end
+ end 
 end
 
 def get_average_age_for_season(data, season)
